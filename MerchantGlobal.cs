5106b4fa4241b5a66f1269a78520d0ea55de66ef
Exterminatorx99 <kristijan.kos99@gmail.com>
2021-10-10 22:31:14 +0200

    Now also sells Mana Crystals, but everything is more expensive.

--- MerchantGlobal.cs
+++ MerchantGlobal.cs
@@ -2,17 +2,25 @@
 
 public class MerchantGlobal : GlobalNPC
 {
+	private readonly int[] MerchantItems =
+	{
+		ItemID.LifeCrystal,
+		ItemID.ManaCrystal
+	};
+
 	public override void SetupShop(int type, Chest shop, ref int nextSlot)
 	{
 		switch (type)
 		{
 			case NPCID.Merchant:
-			{
-				Item item = shop.item[nextSlot++];
-				item.SetDefaults(ItemID.LifeCrystal);
-				item.value = Item.buyPrice(gold: 5);
+				foreach (int itemType in MerchantItems)
+				{
+					Item item = shop.item[nextSlot++];
+					item.SetDefaults(itemType);
+					item.shopCustomPrice = Item.buyPrice(gold: 10);
+				}
+
 				break;
-			}
 		}
 	}
 }
