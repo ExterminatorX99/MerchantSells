be621830b6ad2829ed6c26d04d82b09201729b88
Exterminatorx99 <kristijan.kos99@gmail.com>
2021-10-10 22:45:32 +0200

    Shouldn't be instanced + fancier

--- MerchantGlobal.cs
+++ MerchantGlobal.cs
@@ -1,26 +1,38 @@
-﻿namespace MerchantSells;
+﻿using System.Collections.Generic;
+
+namespace MerchantSells;
 
 public class MerchantGlobal : GlobalNPC
 {
-	private readonly int[] MerchantItems =
+	public static Dictionary<int, int[]> NPCIDToItemsSold;
+
+	public override void Load()
 	{
-		ItemID.LifeCrystal,
-		ItemID.ManaCrystal
-	};
+		NPCIDToItemsSold = new()
+		{
+			[NPCID.Merchant] = new int[]
+			{
+				ItemID.LifeCrystal,
+				ItemID.ManaCrystal,
+			},
+		};
+	}
+
+	public override void Unload()
+	{
+		NPCIDToItemsSold = null!;
+	}
 
 	public override void SetupShop(int type, Chest shop, ref int nextSlot)
 	{
-		switch (type)
-		{
-			case NPCID.Merchant:
-				foreach (int itemType in MerchantItems)
-				{
-					Item item = shop.item[nextSlot++];
-					item.SetDefaults(itemType);
-					item.shopCustomPrice = Item.buyPrice(gold: 10);
-				}
+		if (!NPCIDToItemsSold.TryGetValue(type, out int[] itemTypes))
+			return;
 
-				break;
+		foreach (int itemType in itemTypes)
+		{
+			Item item = shop.item[nextSlot++];
+			item.SetDefaults(itemType);
+			item.shopCustomPrice = Item.buyPrice(gold: 10);
 		}
 	}
 }
